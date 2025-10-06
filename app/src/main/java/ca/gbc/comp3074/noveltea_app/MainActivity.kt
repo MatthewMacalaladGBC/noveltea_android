15dd541c4e29d846bcb7e7f95c6beac55d0051d3
MatthewMacalaladGBC <matthew.macalalad@georgebrown.ca>
2025-10-05 21:05:27 -0400

    barebones draft of Profile page - need to populate upper fields

--- app/src/main/java/ca/gbc/comp3074/noveltea_app/MainActivity.kt
+++ app/src/main/java/ca/gbc/comp3074/noveltea_app/MainActivity.kt
@@ -3,6 +3,7 @@
 package ca.gbc.comp3074.noveltea_app
 
 import android.os.Bundle
+import android.security.identity.AccessControlProfile
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,10 +25,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -66,11 +72,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Noveltea_appTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding)) {
-                        AppNavHost()
-                    }
-                }
+                AppNavHost()
             }
         }
     }
@@ -120,6 +122,7 @@ fun BookGridHomeScreen(navController: NavHostController, books: List<Book>) {
                                MaterialTheme.colorScheme.primary,
                                CircleShape
                            )
+                           .clickable { navController.navigate("profile") }
                    )
                    // Search text box - not interactable as of now (placeholder)
                    Box(
@@ -247,6 +250,90 @@ fun BookDetailScreen(navController: NavHostController, bookId: Int, books: List<
     }
 }
 
+// Profile page screen to display profile info
+// For the mock-up, will be populated with static dummy info
+// Actual app will make use of dataset that holds user info
+// Books listed in mock-up are just the ones from the mock dataset,
+// but each user will hold their own list of books in actuality
+@Composable
+fun ProfileScreen(navController: NavHostController, books: List<Book>) {
+    // Same Scaffold topBar as the Details screen (find a way to reduce redundancy later)
+    Scaffold(
+        topBar = {
+            // Center aligned top bar instead of the default small top bar
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(text = "Noveltea", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+                },
+                // Back arrow icon to take you back to the Home screen
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back button, return to Home screen")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2), // 2 set columns
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+            verticalArrangement = Arrangement.spacedBy(40.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            item(span = { GridItemSpan(2) }) {
+                Column(
+                    modifier = Modifier.fillMaxWidth().padding(innerPadding),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.height(200.dp)
+                    ) {
+                        Text(text = "Row for profile image and User Name")
+                    }
+                    Row(
+                        modifier = Modifier.height(100.dp)
+                    ) {
+                        Text(text = "Row for profile info - followers, following, reviews")
+                    }
+
+                }
+            }
+
+            // Passes list of book items to be used in the grid
+            items(books) { book ->
+                Column(
+                    modifier = Modifier.width(150.dp)
+                        // On click, uses navController to navigate to BookDetailScreen composable
+                        // Passes id of the clicked book to display proper information
+                        .clickable { navController.navigate("detail/${book.id}") },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Surface(
+                        modifier = Modifier.size(width = 150.dp, height = 225.dp).padding(bottom = 8.dp),
+                        shadowElevation = 12.dp
+                    ) {
+                        AsyncImage(
+                            model = book.coverImgUrl,
+                            contentDescription = "Cover image for ${book.title}"
+                        )
+                    }
+                    Text(text = book.title, fontWeight = FontWeight.Bold)
+                    Text(text = book.author, fontStyle = FontStyle.Italic)
+                }
+            }
+        }
+    }
+}
+
 // Later on, maybe have different files for each composable?
 // At the very least, have AppNavHost as it's own (Separation of Concerns)
 @Composable
@@ -264,5 +351,6 @@ fun AppNavHost(){
             val bookId = backstackEntry.arguments?.getString("bookId")?.toIntOrNull() ?: 0
             BookDetailScreen(navController = navController, bookId = bookId, books = books)
         }
+        composable("profile") { ProfileScreen(navController = navController, books) }
     }
 }
