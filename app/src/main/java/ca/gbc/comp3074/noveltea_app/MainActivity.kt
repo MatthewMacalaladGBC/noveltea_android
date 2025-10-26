8f3d0f50527fa88a5560f1eecd2f6da52560c9e4
MatthewMacalaladGBC <matthew.macalalad@georgebrown.ca>
2025-10-26 11:28:17 -0400

    minor ui update; added buffer to top of home screen to avoid overlay with Android status bar

--- app/src/main/java/ca/gbc/comp3074/noveltea_app/MainActivity.kt
+++ app/src/main/java/ca/gbc/comp3074/noveltea_app/MainActivity.kt
@@ -16,12 +16,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
@@ -113,7 +116,10 @@ fun BookGridHomeScreen(navController: NavHostController, books: List<Book>) {
 
     Scaffold(
         topBar = {
-            Column {
+            Column(
+                modifier = Modifier
+                    .windowInsetsPadding(WindowInsets.statusBars)
+            ) {
                 // Row 1: title left, login right
                 Row(
                     modifier = Modifier
